20c9b067495cf4f6c56c013b3141ec6a5297454b
John Soto <soto.ja415@gmail.com>
2024-05-08 01:57:58 -0400

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub struct CPU {
     pub register_a: u8,
+    pub register_x: u8,
     pub status: u8,
     pub program_counter: u8,
 }
@@ -7,8 +8,33 @@ impl CPU {
     pub fn new() -> CPU {
         CPU {
             register_a: 0,
+            register_x: 0,
             status: 0,
             program_counter: 0,
+        }
+    }
+
+    fn lda(&mut self, value: u8) {
+        self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a)
+    }
+
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x)
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        self.status = if result == 0 {
+            self.status | 0b0000_0010
+        } else {
+            self.status & 0b1111_1101
+        };
+
+        self.status = if result & 0b1000_0000 != 0 {
+            self.status | 0b1000_0000
+        } else {
+            self.status & 0b0111_1111
         }
     }
 
@@ -22,25 +48,15 @@ impl CPU {
             match ops_code {
                 0xA9 => {
                     let param = program[self.program_counter as usize];
-                    self.register_a = param;
-
                     self.program_counter += 1;
 
-                    if self.status == 0 {
-                        self.status = self.status | 0b0000_0010;
-                    } else {
-                        self.status = self.status & 0b1111_1101;
-                    }
+                    self.lda(param);
+                }
 
-                    if self.register_a & 0b1000_0000 != 0 {
-                        self.status = self.status | 0b1000_0000;
-                    } else {
-                        self.status = self.status & 0b0111_1111;
-                    }
-                }
-                0x00 => {
-                    return;
-                }
+                0xAA => self.tax(),
+
+                0x00 => return,
+
                 _ => todo!(),
             };
         }
@@ -67,9 +83,20 @@ mod test {
     #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
+
         cpu.interpret(vec![0xa9, 0x00, 0x00]);
 
         // status = 0b0000_0010 == 2
         assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+
+        cpu.register_a = 10;
+        cpu.interpret(vec![0xaa, 0x00]);
+
+        assert_eq!(cpu.register_x, 10);
     }
 }
